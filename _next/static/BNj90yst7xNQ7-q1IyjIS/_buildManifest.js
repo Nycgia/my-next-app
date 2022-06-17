b7fd8c96aed247ead02c42b816123895498902bc
Nycgia <pmunozb@protonmail.com>
2022-06-17 01:09:20 +0000

    Deploying to gh-pages from @ Nycgia/my-next-app@77fae6723c5e0ff3e048ebf930d74b4f59312aa9 🚀

---
+++ _next/static/BNj90yst7xNQ7-q1IyjIS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/149b18973e5508c7.css","static/chunks/pages/index-efc1cade60c69162.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],"/home":["static/chunks/pages/home-a9fe9ebb72a12283.js"],sortedPages:["/","/_app","/_error","/home"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
